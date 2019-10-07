de728ab365655e252f4197fcdde4672d6fb73c27
Hannah Mahon <mahonha@tcd.ie>
2019-10-07 13:57:01 +0100

    tested for an empty tree

--- LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -1,13 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class LowestCommonAncestorTest {
-
+public class LowestCommonAncestorTest 
+{
 	@Test
-	public void testEmpty() {
-		LowestCommonAncestor LCA = new LowestCommonAncestor();
-		assertNull(LCA.LCA(LCA.root, 6, 3));
+	public void testEmpty() 
+	{
+		LowestCommonAncestor.BinaryTree tree = new LowestCommonAncestor.BinaryTree();
+		assertEquals(tree.findLCA(1,2), -1);
+		
+		assertEquals(tree.findLCA(2, 4), -1);
+		
+		
+		
 	}
+	
 
 }

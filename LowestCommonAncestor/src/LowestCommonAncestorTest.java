dbdba2ac9eb1776f10e173823bd34888c1a82ac3
Hannah Mahon <mahonha@tcd.ie>
2019-10-07 14:21:37 +0100

    adding testing for 2 nodes

--- LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -12,8 +12,20 @@ public class LowestCommonAncestorTest
 		
 		assertEquals(tree.findLCA(2, 4), -1);
 		
+	}
+	
+	@Test
+	public void testForTwoNodes()
+	{
+		LowestCommonAncestor.BinaryTree tree = new LowestCommonAncestor.BinaryTree();
+		tree.root =  new LowestCommonAncestor.Node(1);
+		tree.root.left = new LowestCommonAncestor.Node(2);
+		assertEquals(tree.findLCA(1,2), 1);
 		
-		
+		LowestCommonAncestor.BinaryTree treeTwo = new LowestCommonAncestor.BinaryTree();
+		treeTwo.root = new LowestCommonAncestor.Node(1);
+		treeTwo.root.right = new LowestCommonAncestor.Node(3);
+		assertEquals(tree.findLCA(1, 3), 1);
 	}
 	
 

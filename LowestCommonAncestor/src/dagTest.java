32cb66d06e3cfb74eec85b4e5df889f0a8868c89
Hannah Mahon <mahonha@tcd.ie>
2019-10-16 22:56:33 +0100

    tested for lca in dag

--- LowestCommonAncestor/src/dagTest.java
+++ LowestCommonAncestor/src/dagTest.java
@@ -30,7 +30,7 @@ public class dagTest {
 	
 	
 	@Test 
-	public void testIndegree()
+	public void testIndegree() //testing for paths
 	{
 		dag dag = new dag(10);
 		dag.addEdge(1, 2);
@@ -69,6 +69,38 @@ public class dagTest {
 		
 	}
 
+	@Test 
+	public void testLCA()
+	{
+		dag dag = new dag(10);
+		dag.addEdge(0, 1);
+		dag.addEdge(1, 2);
+		dag.addEdge(1, 3);
+		dag.addEdge(2, 5);
+		dag.addEdge(3, 4);
+		dag.addEdge(4, 6);
+		dag.addEdge(5, 6);
+		dag.addEdge(6, 8);
+		dag.addEdge(5, 7);
+		dag.addEdge(7, 8);
+		dag.addEdge(8, 9);
+		
+		assertEquals(7, dag.findLCA(8, 7));
+		assertEquals(7, dag.findLCA(7, 9));
+		assertEquals(3, dag.findLCA(3, 4));
+	}
+	
+
+	@Test
+	public void testLCASameVertex()
+	{
+		dag dag = new dag(5);
+		dag.addEdge(0, 1);
+		dag.addEdge(0, 2);
+		dag.addEdge(1, 2);
+		
+		assertEquals(2, dag.findLCA(2,2));
+	}
 	
 
 }
